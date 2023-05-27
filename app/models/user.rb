d60a11f2b2239a2a87e761d0e568d2606a376329
taki39384 <132125615+taki39384@users.noreply.github.com>
2023-05-27 13:45:14 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/, message: "must be alphanumeric and include at least one letter and one number" }
+  validates :password_confirmation, presence: true
+  validate :password_match
+  
+  private
+  def password_match
+    errors.add(:password_confirmation, "doesn't match Password") if password != password_confirmation
+  end
 end
