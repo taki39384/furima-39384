6863590abf6b7dcdb16a7fbb7383fb49386901c9
taki39384 <132125615+taki39384@users.noreply.github.com>
2023-05-28 01:17:05 +0900

    テストコード修正＆バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_day, presence: true
   validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/, message: "must be alphanumeric and include at least one letter and one number" }
   validates :password_confirmation, presence: true
   validate :password_match
