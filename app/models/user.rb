13d5514052a4bf05789475820a28402b450b3d28
taki39384 <132125615+taki39384@users.noreply.github.com>
2023-05-28 17:27:32 +0900

    バリデーションとマイグレーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "は全角で入力してください" }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "は全角で入力してください" }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ヶ]/, message: "は全角で入力してください" }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ヶ]/, message: "は全角で入力してください" }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
   validates :birth_day, presence: true
--- db/migrate/20230526081114_devise_create_users.rb
+++ db/migrate/20230526081114_devise_create_users.rb
@@ -10,7 +10,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :last_name_kana,     null: false
       t.string :first_name_kana,    null: false
       t.date   :birth_day,          null: false
-      t.string :email,              null: false, unique: true, default: ""
+      t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
