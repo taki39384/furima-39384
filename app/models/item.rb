d8b9afeaa73231921d60a8d1fdf3ba38ba7c7af3
taki39384 <132125615+taki39384@users.noreply.github.com>
2023-05-29 13:26:40 +0900

    itemバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
   has_one_attached :itemimage
   belongs_to :user
+
+  validates :name, presence: true
+  validates :category_id, presence: true, numericality: { only_integer: true }
+  validates :explanation, presence: true
+  validates :situation_id, presence: true, numericality: { only_integer: true }
+  validates :delivery_charge_id, presence: true, numericality: { only_integer: true }
+  validates :region_of_origin_id, presence: true, numericality: { only_integer: true }
+  validates :day_to_ship_id, presence: true, numericality: { only_integer: true }
+  validates :price, presence: true, numericality: { only_integer: true }
+  validates :user, presence: true
 end
