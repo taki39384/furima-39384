e5b977e9b0c4b26b6db653e557de0aee5578d039
taki39384 <132125615+taki39384@users.noreply.github.com>
2023-05-31 21:29:39 +0900

    商品編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit, :update]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -17,6 +18,19 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+    redirect_to item_path
+    else 
+      render :edit
+    end
+  end
   
   def show
     @item = Item.find(params[:id])
@@ -27,5 +41,10 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:itemimage, :name, :explanation, :category_id, :situation_id, :delivery_charge_id, :region_of_origin_id, :day_to_ship_id, :price).merge(user_id: current_user.id)
   end
-
+  def move_to_index
+    return if @item.nil?
+    unless current_user.id == @item.user.id
+      redirect_to root_path
+    end
+  end
 end
