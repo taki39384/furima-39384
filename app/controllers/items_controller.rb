5cb03005a44daf09b47958cf8743f005762e966c
taki39384 <132125615+taki39384@users.noreply.github.com>
2023-06-01 14:42:05 +0900

    レビュー修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update]
+  before_action :set_item, only: [:edit, :update, :show]
   before_action :move_to_index, except: [:index, :show]
-
+  
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -20,12 +21,10 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
     redirect_to item_path
     else 
@@ -34,7 +33,6 @@ class ItemsController < ApplicationController
   end
   
   def show
-    @item = Item.find(params[:id])
   end
 
 
@@ -42,10 +40,14 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:itemimage, :name, :explanation, :category_id, :situation_id, :delivery_charge_id, :region_of_origin_id, :day_to_ship_id, :price).merge(user_id: current_user.id)
   end
+
   def move_to_index
-    return if @item.nil?
     unless current_user && current_user.id == @item.user.id
       redirect_to root_path
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
