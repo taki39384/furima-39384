0bdedfdcc1750c4a6231718538cd79d1854a8016
taki39384 <132125615+taki39384@users.noreply.github.com>
2023-06-02 18:58:34 +0900

    購入のためのフォームを実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,23 @@
 class OrdersController < ApplicationController
   def index
+    @order_information = OrderInformation.new
+    @item = Item.find(params[:item_id])
+  end
 
+  def create
+    @item = Item.find(params[:item_id])
+    @order_information = OrderInformation.new(order_information_params)
+    if @order_information.valid?
+      @order_information.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_information_params
+    params.require(:order_information).permit(:post_code, :region_of_origin_id, :municipalities, :address, :building_name, :number, :order).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
---
+++ app/models/OrderInformation.rb
@@ -0,0 +1,22 @@
+class OrderInformation
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :post_code, :region_of_origin_id, :municipalities, :address, :building_name, :number, :order
+
+  # ここにバリデーションの処理を書く
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :post_code
+    validates :region_of_origin_id
+    validates :municipalities
+    validates :address
+    validates :number
+    validates :order
+  end
+
+  def save
+    # 各テーブルにデータを保存する処理を書く
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Information.create(post_code: post_code, region_of_origin_id: region_of_origin_id, municipalities: municipalities, address: address, building_name: building_name, number: number, order_id: order.id)
+  end
+end
