fe8c99b151c784d99dd0819ae99db36a7ed74f96
taki39384 <132125615+taki39384@users.noreply.github.com>
2023-06-04 15:25:31 +0900

    ページ遷移修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,9 @@ class OrdersController < ApplicationController
   def index
     @order_information = OrderInformation.new
     @item = Item.find(params[:item_id])
+    if current_user == @item.user
+      redirect_to root_path
+    end
   end
 
   def create
