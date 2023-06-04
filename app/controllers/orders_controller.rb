24393835e36dc0b762c73260e8b00ff1c8bf8c1d
taki39384 <132125615+taki39384@users.noreply.github.com>
2023-06-04 15:18:46 +0900

    クレジットカード機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+
   def index
     @order_information = OrderInformation.new
     @item = Item.find(params[:item_id])
@@ -8,6 +10,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_information = OrderInformation.new(order_information_params)
     if @order_information.valid?
+      pay_item
       @order_information.save
       redirect_to root_path
     else
@@ -18,6 +21,15 @@ class OrdersController < ApplicationController
   private
 
   def order_information_params
-    params.require(:order_information).permit(:post_code, :region_of_origin_id, :municipalities, :address, :building_name, :number, :order).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:order_information).permit(:post_code, :region_of_origin_id, :municipalities, :address, :building_name, :number, :order).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  
+      Payjp::Charge.create(
+        amount: @item[:price],
+        card: order_information_params[:token],
+        currency: 'jpy'
+      )
   end
 end
