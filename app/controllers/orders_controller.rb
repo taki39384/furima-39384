3bd228b1bdf8e4f9f10352350b7da04718cd8368
taki39384 <132125615+taki39384@users.noreply.github.com>
2023-06-04 19:35:01 +0900

    スペルミス修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
-  before_action :item, onle: [:index, :create]
+  before_action :item, only: [:index, :create]
 
   def index
     @order_information = OrderInformation.new
