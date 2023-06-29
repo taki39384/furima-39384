f4c8c7ecca3cf95e3a6703b1f93370de2a3900dd
taki39384 <132125615+taki39384@users.noreply.github.com>
2023-06-29 17:39:19 +0900

    プレビュー機能の実装

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -2,5 +2,22 @@ document.addEventListener('DOMContentLoaded', function(){
   const postForm = document.getElementById('new_post');
   const previewList = document.getElementById('previews');
   if (!postForm) return null;
-  console.log("preview.jsが読み込まれました");
+
+  const fileField = document.querySelector('input[type="file"][name="item[itemimage]"]');
+  fileField.addEventListener('change', function(e){
+    const alreadyPreview = document.querySelector('.preview');
+    if (alreadyPreview) {
+      alreadyPreview.remove();
+    };
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('src', blob);
+
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
+  });
 });
